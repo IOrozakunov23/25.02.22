978228c2eb62a3eea35d0a91fa42b83ce5b7a670
IOrozakunov23 <islamburunduk045>
2022-02-25 14:46:32 +0300

    added 2 files

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let article1 = document.getElementById("article1");
-article1.style.display = "none";
-document.getElementById("art1").addEventListener("click", function () {
-  article1.style.display = "block";
+let example2 = document.getElementById("example-1");
+example2.style.display = "none";
+document.getElementById("img1").addEventListener("click", function () {
+  example1.style.display = "block";
 });
-document.getElementById("art1").addEventListener("dblclick", function () {
-  article1.style.display = "none";
+document.getElementById("img1").addEventListener("dblclick", function () {
+  example1.style.display = "none";
 });
 
-let article2 = document.getElementById("article2");
-article2.style.display = "none";
-document.getElementById("art2").addEventListener("click", function () {
-  article2.style.display = "block";
+let example2 = document.getElementById("example2");
+example2.style.display = "none";
+document.getElementById("img2").addEventListener("click", function () {
+   example2.style.display = "block";
 });
-document.getElementById("art2").addEventListener("dblclick", function () {
-  article2.style.display = "none";
+document.getElementById("img2").addEventListener("dblclick", function () {
+  example2.style.display = "none";
 });
 
 let article3 = document.getElementById("article3");
