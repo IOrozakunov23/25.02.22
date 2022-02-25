d88eaa198d2cf933c22b1a52c09e44997ef3d677
IOrozakunov23 <islamburunduk045>
2022-02-25 14:59:55 +0300

    added 2 files

--- script.js
+++ script.js
@@ -10,17 +10,17 @@ document.getElementById("img1").addEventListener("dblclick", function () {
 let example2 = document.getElementById("example2");
 example2.style.display = "none";
 document.getElementById("img2").addEventListener("click", function () {
-   example2.style.display = "block";
+  example2.style.display = "block";
 });
 document.getElementById("img2").addEventListener("dblclick", function () {
   example2.style.display = "none";
 });
 
-let article3 = document.getElementById("article3");
-article3.style.display = "none";
-document.getElementById("art3").addEventListener("click", function () {
-  article3.style.display = "block";
+let example3 = document.getElementById("example3");
+example3.style.display = "none";
+document.getElementById("img3").addEventListener("click", function () {
+  example3.style.display = "block";
 });
 document.getElementById("art3").addEventListener("dblclick", function () {
-  article3.style.display = "none";
+  example3.style.display = "none";
 });
