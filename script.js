2d99ee8a8358e0665977d414583fe5c5a4c39300
IOrozakunov23 <islamburunduk045>
2022-02-25 15:39:16 +0300

    added 2 files

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-let example2 = document.getElementById("example-1");
-example2.style.display = "none";
-document.getElementById("img1").addEventListener("click", function () {
-  example1.style.display = "block";
-});
-document.getElementById("img1").addEventListener("dblclick", function () {
-  example1.style.display = "none";
-});
-
-let example2 = document.getElementById("example2");
-example2.style.display = "none";
-document.getElementById("img2").addEventListener("click", function () {
-  example2.style.display = "block";
-});
-document.getElementById("img2").addEventListener("dblclick", function () {
-  example2.style.display = "none";
-});
-
-let example3 = document.getElementById("example3");
-example3.style.display = "none";
-document.getElementById("img3").addEventListener("click", function () {
-  example3.style.display = "block";
-});
-document.getElementById("art3").addEventListener("dblclick", function () {
-  example3.style.display = "none";
+let details = document.querySelectorAll('details');
+details.forEach((detail) => {
+  detail.addEventListener('toggle', function (e) {
+    let article = document.querySelector(${e.torget.id});
+    article.style.display = "none";
+  });
 });
