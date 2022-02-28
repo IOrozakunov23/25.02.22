c5e5d0a74d640f65fdf28973337207305a1c6bd3
IOrozakunov23 <islamburunduk045>
2022-02-28 14:49:10 +0300

    added 2 files

--- script1.js
+++ script1.js
@@ -1,2 +1,13 @@
-let fruist = ["apple", "banana", "qiwi,"]
-fruist.forEach(element => console.log(element));  
+  // let fruist = ["apple", "banana", "qiwi,"]
+  // fruist.forEach(element => console.log(element)); 
+
+let fruit = ["apple",
+"banana", 
+false,
+13,
+"hello",
+["1","2","3"],
+{ name: "Peter", lastname: "Parker"},
+12345]
+
+array.forEach(element => console.log(element));
