e18606547dceafa4b4a3f25f5de9d6608d5dbbbb
IOrozakunov23 <islamburunduk045>
2022-02-28 15:56:26 +0300

    added 2 files

--- script1.js
+++ script1.js
@@ -1,13 +1,50 @@
-  // let fruist = ["apple", "banana", "qiwi,"]
-  // fruist.forEach(element => console.log(element)); 
+// // let fruist = ["apple", "banana", "qiwi,"]
+// // fruist.forEach(element => console.log(element));
 
-let fruit = ["apple",
-"banana", 
-false,
-13,
-"hello",
-["1","2","3"],
-{ name: "Peter", lastname: "Parker"},
-12345]
+// let fruit = [
+//   "apple",
+//   "banana",
+//   false,
+//   13,
+//   "hello",
+//   ["1", "2", "3"],
+//   { name: "Peter", lastname: "Parker" },
+//   12345,
+// ];
 
-array.forEach(element => console.log(element));
+// Array.prototype.forEach { 
+//    var a = ["a", "b", "c"];
+//  a.forEach(function(entry) {
+//      console.log(entry);
+// });
+
+let fruits = [
+  {
+    name: "Peter",
+    lastname: "Parker",
+    age: "34"
+  },
+  {
+    name: "Dog",
+    lastname: "Cat",
+    age: "34"
+  },
+  {
+    name: "Bird",
+    lastname: "Lion",
+    age: "34"
+  },
+  {
+    name: "Wolf",
+    lastname: "Bear",
+    age: "34"
+  },
+  {
+    name: "Shark",
+    lastname: "Fish",
+    age: "34"
+  }
+]
+let container = document.quarySelector("#container")
+
+people.forEach((item)) =>
